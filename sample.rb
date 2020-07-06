5f3860a53237cb69453df2076930130189186a1b
vinamra28 <vinjain@redhat.com>
2020-07-06 17:37:53 +0530

    Add rubocop config file

--- sample.rb
+++ sample.rb
@@ -1,9 +1,5 @@
-# Variables and expressions.
 a = 10
 b = 3 * a + 2
-printf("%d %d\n", a, b);
+c = 10 * b
 
-# Type is dynamic.
-b = "A string"
-c = 'Another String'
-print b + " and " + c + "\n"
+puts c
